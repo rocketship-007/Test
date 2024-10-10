846a6a17a911d9017171787808557015364da028
rocketship-007 <arnaavdhuri@gmail.com>
2024-10-09 19:37:20 -0500

    added eat

---
+++ hungry.py
@@ -0,0 +1,5 @@
+a = str(input("Are you hungry"))
+if a == "yes":
+    print("Eat")
+else:
+    print("Do your homework! :)")
--- rnumber.py
+++ rnumber.py
@@ -1,4 +1,4 @@
-a = str(input("Do you want a snack?"))
+a = str(input("Do you want a snack"))
 if a == "yes":
     print("eat something")
 else:
